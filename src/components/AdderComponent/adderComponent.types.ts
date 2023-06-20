c39a74fa24296ee0a0a6fc386abc96e3c8d31f57
scrabwind <scrabwind@gmail.com>
2023-06-20 18:04:03 +0200

    Update packages

--- src/components/AdderComponent/adderComponent.types.ts
+++ src/components/AdderComponent/adderComponent.types.ts
@@ -2,4 +2,4 @@ import { specificConst } from '@/components/AdderComponent/adderComponent.const'
 
 // https://www.typescriptlang.org/docs/handbook/enums.html#objects-vs-enums
 
-export type SpecificConst = typeof specificConst[keyof typeof specificConst]
+export type SpecificConst = (typeof specificConst)[keyof typeof specificConst]
